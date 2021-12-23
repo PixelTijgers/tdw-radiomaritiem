95f4ceaefe3f8a2bba162cc755ce02673f176c77
PixelTijgers <57810027+PixelTijgers@users.noreply.github.com>
2021-12-23 13:15:59 +0100

    Temporary removed the disclaimer.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
 
     <div class="text-center text-sm	 py-3">
 
-        <p>&#169 {{ date('Y') }}, {{ env('APP_NAME') }} | <a href="/disclaimer">Disclaimer</a></p>
+        <p>&#169 {{ date('Y') }}, {{ env('APP_NAME') }} | <a href="/">Disclaimer</a></p>
 
     </div>
 
