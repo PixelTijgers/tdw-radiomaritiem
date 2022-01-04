fbce36048cbfe9e937d4ee1f8e0e5123c1d0e9ec
PixelTijgers <57810027+PixelTijgers@users.noreply.github.com>
2022-01-04 15:38:01 +0100

    Minor changes.

--- resources/views/includes/hosts.blade.php
+++ resources/views/includes/hosts.blade.php
@@ -4,43 +4,6 @@
 
         <figure class="md:flex md:justify-end lg:w-4/12 lg:translate-top translate-right">
 
-            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/bert-van-steenberghe.png') }}" alt="Foto van Bert Van Steenberghe" />
-
-        </figure>
-
-        <div class="lg:w-8/12">
-
-            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Bert Van Steenberghe</h3>
-
-            <p class="text-base lg:text-lg text-right lg:text-left">Ook Bert is radiopresentator bij Studio Brussel. Hij kan z’n kennis en passie voor muziek ook kwijt als DJ. Zijn stem leent hij dan weer regelmatig uit voor spots en aankondigingen op VRT. Bert is niet zomaar een presentator, meer een radiomaker.</p>
-
-        </div>
-
-
-    </div>
-
-    <div class="lg:flex -mt-16">
-
-        <figure class="lg:w-4/12 translate-left lg:translate-top">
-
-            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/michele-cuvelier.png') }}" alt="Foto van Michèle Cuvelier" />
-
-        </figure>
-
-        <div class="lg:w-8/12">
-
-            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 lg:text-right">Michèle Cuvelier</h3>
-
-            <p class="lg:text-lg lg:text-right">Nog zo’n mooie en bekende StuBru stem! Michèle neemt regelmatig het ochtendblok voor haar rekening, en presenteert inmiddels elke weekdag haar <strong>Fanclub</strong>. Recent maakte ze de sprong naar televisie en presenteerde ze De tijdloze op Canvas.</p>
-
-        </div>
-
-    </div>
-
-    <div class="lg:flex -mt-16 flex-row-reverse">
-
-        <figure class="md:flex md:justify-end lg:w-4/12 lg:translate-top translate-right">
-
             <img class="md:w-8/12 lg:w-full" src="{{ asset('img/xavier-taveirne.png') }}" alt="Foto van Xavier Taveirne" />
 
         </figure>
@@ -49,7 +12,7 @@
 
             <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Xavier Taveirne</h3>
 
-            <p class="lg:text-lg text-right lg:text-left">Xavier is een voorname en bekende VRT-stem. Hij is journalist, presentator, TV-maker, nieuwslezer en slimste mens. Hij presenteert op dit moment <strong>De ochtend</strong> op Radio 1 en is ook een van de gezichten van Het Journaal op VRT. </p>
+            <p class="lg:text-lg text-right lg:text-left">Xavier is een voorname en bekende VRT-stem. Hij is journalist, presentator, TV-maker, nieuwslezer en slimste mens. Hij presenteert op dit moment ‘De ochtend’ op Radio 1 en is ook een van de gezichten van Het Journaal op VRT. </p>
 
         </div>
 
@@ -77,15 +40,52 @@
 
         <figure class="md:flex md:justify-end lg:w-4/12 lg:translate-top translate-right">
 
+            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/bert-van-steenberghe.png') }}" alt="Foto van Bert Van Steenberghe" />
+
+        </figure>
+
+        <div class="lg:w-8/12">
+
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Bert Van Steenberghe</h3>
+
+            <p class="text-base lg:text-lg text-right lg:text-left">Ook Bert is radiopresentator bij Studio Brussel. Hij kan z’n kennis en passie voor muziek ook kwijt als DJ. Zijn stem leent hij dan weer regelmatig uit voor spots en aankondigingen op VRT. Bert is niet zomaar een presentator, meer een radiomaker.</p>
+
+        </div>
+
+    </div>
+
+    <div class="lg:flex -mt-16">
+
+        <figure class="lg:w-4/12 translate-left lg:translate-top">
+
             <img class="md:w-8/12 lg:w-full" src="{{ asset('img/dorien-leyers.png') }}" alt="Foto van Dorien Leyers" />
 
         </figure>
 
         <div class="lg:w-8/12">
 
-            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Dorien Leyers</h3>
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 lg:text-right">Dorien Leyers</h3>
 
-            <p class="text-base lg:text-lg text-right lg:text-left">Dorien is presentatrice en moderator met een eindeloze passie voor events en entertainment. Als vlotte duizendpoot kan ze als geen ander een publiek aanspreken, altijd met een lach, soms met een knipoog. </p>
+            <p class="lg:text-lg lg:text-right">Dorien is presentatrice en moderator met een eindeloze passie voor events en entertainment. Als vlotte duizendpoot kan ze als geen ander een publiek aanspreken, altijd met een lach, soms met een knipoog. </p>
+
+        </div>
+
+    </div>
+
+
+    <div class="lg:flex -mt-16 flex-row-reverse">
+
+        <figure class="md:flex md:justify-end lg:w-4/12 lg:translate-top translate-right">
+
+            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/michele-cuvelier.png') }}" alt="Foto van Michèle Cuvelier" />
+
+        </figure>
+
+        <div class="lg:w-8/12">
+
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Michèle Cuvelier</h3>
+
+            <p class="text-base lg:text-lg text-right lg:text-left">Nog zo’n mooie en bekende StuBru stem! Michèle neemt regelmatig het ochtendblok voor haar rekening, en presenteert inmiddels elke weekdag haar ‘Fanclub’. Recent maakte ze de sprong naar televisie en presenteerde ze De tijdloze op Canvas. </p>
 
         </div>
 
@@ -103,7 +103,7 @@
 
             <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 lg:text-right">Linde Merckpoel</h3>
 
-            <p class="lg:text-lg lg:text-right">Linde Merckpoel was jarenlang dé stem en het gezicht van Studio Brussel. Na haar mooie radiocarrière waagde ze zich, als liefhebber van de <strong>nieuwe media</strong>, aan het vloggerschap. Linde vlogt voor verschillende kanalen van VRT. Voor Radio Mariteam gaat ze op pad als vliegende reporter.</p>
+            <p class="lg:text-lg lg:text-right">Linde Merckpoel was jarenlang dé stem en het gezicht van Studio Brussel. Na haar mooie radiocarrière waagde ze zich, als liefhebber van de ‘nieuwe media’, aan het vloggerschap. Linde vlogt voor verschillende kanalen van VRT. Voor Radio Mariteam gaat ze op pad als vliegende reporter.</p>
 
         </div>
 
@@ -121,7 +121,7 @@
 
             <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Ine Beyen</h3>
 
-            <p class="text-base lg:text-lg text-right lg:text-left">Ine Beyen is ex-profwielrenster en bleef ook na haar carrière vertoeven in de sportwereld. Ze maakt videoreportages, zowel voor onlineplatforms als voor televisie. Bij het grote publiek werd ze bekend door haar videorubriek in het populaire Eén programma <strong>Vive le Vélo</strong>. Voor Radio Mariteam is Ine fietsreporter. Ze volgt een  pelotonnetje van werknemers tussen Port of Antwerp en Port of Zeebrugge.</p>
+            <p class="text-base lg:text-lg text-right lg:text-left">Ine Beyen is ex-profwielrenster en bleef ook na haar carrière vertoeven in de sportwereld. Ze maakt videoreportages, zowel voor onlineplatforms als voor televisie. Bij het grote publiek werd ze bekend door haar videorubriek in het populaire Eén programma ‘Vive le Vélo’. Voor Radio Mariteam is Ine fietsreporter. Ze volgt een  pelotonnetje van werknemers tussen Port of Antwerp en Port of Zeebrugge.</p>
 
         </div>
 
@@ -139,7 +139,7 @@
 
             <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 lg:text-right">Nico Mattan</h3>
 
-            <p class="lg:text-lg lg:text-right">Nico Mattan is een populaire ex-profwielrenner en bleef actief in de wielerwereld als ploegleider en adviseur. Zijn carrière bracht hem van het beklimmen van de Kwaremont naar het ambassadeurschap van het Kwaremontbier. Ook hij zal op z’n eigen, onnavolgbare manier verslag uitbrengen van de <strong>Radio Mariteam</strong> fietsrit. </p>
+            <p class="lg:text-lg lg:text-right">Nico Mattan is een populaire ex-profwielrenner en bleef actief in de wielerwereld als ploegleider en adviseur. Zijn carrière bracht hem van het beklimmen van de Kwaremont naar het ambassadeurschap van het Kwaremontbier. Ook hij zal op z’n eigen, onnavolgbare manier verslag uitbrengen van de ‘Radio Mariteam’ fietsrit. </p>
 
         </div>
 
