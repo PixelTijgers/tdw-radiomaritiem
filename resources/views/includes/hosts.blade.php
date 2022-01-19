1052874ef3fbe114ee039421995ce91f96f96d27
PixelTijgers <57810027+PixelTijgers@users.noreply.github.com>
2022-01-19 08:08:20 +0100

    Added minor information.

--- resources/views/includes/hosts.blade.php
+++ resources/views/includes/hosts.blade.php
@@ -24,15 +24,51 @@
 
         <figure class="lg:w-4/12 translate-left lg:translate-top">
 
+            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/sophie-engelen.png') }}" alt="Foto van Sofie Engelen" />
+
+        </figure>
+
+        <div class="lg:w-8/12">
+
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 lg:text-right">Sofie Engelen</h3>
+
+            <p class="lg:text-lg lg:text-right">Sofie is presentatrice en redactrice, ze werkte voor Studio Brussel waar ze als reporter werd ingezet op grote festivals en ze elke zondagavond haar eigen programma “Engelen en Chedraoui” presenteerde. Bij TMF mocht ze als VJ haar grote helden interviewen, zoals Foo Fighters, MGMT, The Killers en Kings of Leon. In 2019 richtte ze mee de nieuwe radiozender Willy op, een rockzender met een hoek af. </p>
+
+        </div>
+
+    </div>
+
+    <div class="lg:flex -mt-16 flex-row-reverse">
+
+        <figure class="md:flex md:justify-end lg:w-4/12 lg:translate-top translate-right">
+
             <img class="md:w-8/12 lg:w-full" src="{{ asset('img/joris-brys.png') }}" alt="Foto van Joris Brys" />
 
         </figure>
 
         <div class="lg:w-8/12">
 
-            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 lg:text-right">Joris Brys</h3>
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Joris Brys</h3>
 
-            <p class="lg:text-lg lg:text-right">Joris is the new kid on the block. Hij wierp zich de voorbije 2 jaar op als een van de dragers van Studio Brussel. Je kan z’n spontane manier van presenteren ook bewonderen op VIER en Play Sports. </p>
+            <p class="text-base lg:text-lg text-right lg:text-left">Joris is the new kid on the block. Hij wierp zich de voorbije 2 jaar op als een van de dragers van Studio Brussel. Je kan z’n spontane manier van presenteren ook bewonderen op VIER en Play Sports. </p>
+
+        </div>
+
+    </div>
+
+    <div class="lg:flex -mt-16">
+
+        <figure class="lg:w-4/12 translate-left lg:translate-top">
+
+            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/eline-de-munck.png') }}" alt="Foto van Eline De Munck" />
+
+        </figure>
+
+        <div class="lg:w-8/12">
+
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 lg:text-right">Eline De Munck</h3>
+
+            <p class="lg:text-lg lg:text-right">Eline De Munck is presentatrice, actrice, zangeres en bezieler van haar eigen merk Odette Lunettes. Ze begon haar carrière voor televisie bij JIM, werkte als radiohost bij Qmusic en is bovendien bevlogen public speaker.</p>
 
         </div>
 
@@ -56,6 +92,8 @@
 
     </div>
 
+
+
     <div class="lg:flex -mt-16">
 
         <figure class="lg:w-4/12 translate-left lg:translate-top">
@@ -74,20 +112,19 @@
 
     </div>
 
-
     <div class="lg:flex -mt-16 flex-row-reverse">
 
         <figure class="md:flex md:justify-end lg:w-4/12 lg:translate-top translate-right">
 
-            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/michele-cuvelier.png') }}" alt="Foto van Michèle Cuvelier" />
+            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/stijn-van-de-voorde.png') }}" alt="Foto van Stijn Van de Voorde" />
 
         </figure>
 
         <div class="lg:w-8/12">
 
-            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Michèle Cuvelier</h3>
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Stijn Van de Voorde</h3>
 
-            <p class="text-base lg:text-lg text-right lg:text-left">Nog zo’n mooie en bekende StuBru stem! Michèle neemt regelmatig het ochtendblok voor haar rekening, en presenteert inmiddels elke weekdag haar ‘Fanclub’. Recent maakte ze de sprong naar televisie en presenteerde ze De tijdloze op Canvas. </p>
+            <p class="lg:text-lg lg:text-left">Stijn Van de Voorde werkt al meer dan 20 jaar voor Studio Brussel en is naast radiovirtuoos ook ervaren programmamaker. Met twee decennia rock ‘n’ roll-kennis in z’n rugzak vormt deze radioexpert een van de kopstukken van Radio Mariteam. </p>
 
         </div>
 
@@ -115,15 +152,15 @@
 
         <figure class="md:flex md:justify-end lg:w-4/12 lg:translate-top translate-right">
 
-            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/ine-beyen.png') }}" alt="Foto van Ine Beyen" />
+            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/michele-cuvelier.png') }}" alt="Foto van Michèle Cuvelier" />
 
         </figure>
 
         <div class="lg:w-8/12">
 
-            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Ine Beyen</h3>
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Michèle Cuvelier</h3>
 
-            <p class="text-base lg:text-lg text-right lg:text-left">Ine Beyen is ex-profwielrenster en bleef ook na haar carrière vertoeven in de sportwereld. Ze maakt videoreportages, zowel voor onlineplatforms als voor televisie. Bij het grote publiek werd ze bekend door haar videorubriek in het populaire Eén programma ‘Vive le Vélo’. Voor Radio Mariteam is Ine fietsreporter. Ze volgt een pelotonnetje van werknemers van Port of Antwerp naar Port of Zeebrugge.</p>
+            <p class="text-base lg:text-lg text-right lg:text-left">Nog zo’n mooie en bekende StuBru stem! Michèle neemt regelmatig het ochtendblok voor haar rekening, en presenteert inmiddels elke weekdag haar ‘Fanclub’. Recent maakte ze de sprong naar televisie en presenteerde ze De tijdloze op Canvas.</p>
 
         </div>
 
@@ -133,15 +170,33 @@
 
         <figure class="lg:w-4/12 translate-left lg:translate-top">
 
+            <img class="md:w-8/12 lg:w-full" src="{{ asset('img/ine-beyen.png') }}" alt="Foto van Ine Beyen" />
+
+        </figure>
+
+        <div class="lg:w-8/12">
+
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 lg:text-right">Ine Beyen</h3>
+
+            <p class="lg:text-lg lg:text-right">Ine Beyen is ex-profwielrenster en bleef ook na haar carrière vertoeven in de sportwereld. Ze maakt videoreportages, zowel voor onlineplatforms als voor televisie. Bij het grote publiek werd ze bekend door haar videorubriek in het populaire Eén programma ‘Vive le Vélo’. Voor Radio Mariteam is Ine fietsreporter. Ze volgt een  pelotonnetje van werknemers tussen Port of Antwerp en Port of Zeebrugge. </p>
+
+        </div>
+
+    </div>
+
+    <div class="lg:flex -mt-16 flex-row-reverse">
+
+        <figure class="md:flex md:justify-end lg:w-4/12 lg:translate-top translate-right">
+
             <img class="md:w-8/12 lg:w-full" src="{{ asset('img/nico-mattan.png') }}" alt="Foto van Nico Mattan" />
 
         </figure>
 
         <div class="lg:w-8/12">
 
-            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 lg:text-right">Nico Mattan</h3>
+            <h3 class="text-2xl lg:text-4xl lg:mt-16 mb-5 text-right lg:text-left">Nico Mattan</h3>
 
-            <p class="lg:text-lg lg:text-right">Nico Mattan is een populaire ex-profwielrenner en bleef actief in de wielerwereld als ploegleider en adviseur. Zijn carrière bracht hem van het beklimmen van de Kwaremont naar het ambassadeurschap van het Kwaremontbier. Ook hij zal op z’n eigen, onnavolgbare manier verslag uitbrengen van de ‘Radio Mariteam’ fietsrit die start in Zeebrugge. </p>
+            <p class="text-base lg:text-lg text-right lg:text-left">Nico Mattan is een populaire ex-profwielrenner en bleef actief in de wielerwereld als ploegleider en adviseur. Zijn carrière bracht hem van het beklimmen van de Kwaremont naar het ambassadeurschap van het Kwaremontbier. Ook hij zal op z’n eigen, onnavolgbare manier verslag uitbrengen van de ‘Radio Mariteam’ fietsrit. </p>
 
         </div>
 
